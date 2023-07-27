852af1f5f551464497e31130d60017f09f0adbe5
brisk-demos <140211592+brisk-demos@users.noreply.github.com>
2023-07-26 17:19:11 -0700

    Alpha (#6)
    * some initial attempts to change shit
    
    * some initial attempts to change shit - fixing test
    
    * add sudo to apt-get
    
    * rename to install instead of greet
    
    * rename to install instead of greet
    
    * feat: move brisk to /usr/local/bin so it is in the PATH
    
    * move from hello to setup
    
    * add in a version for brisk defaulting to latest
    
    * adding in env vars

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -5,6 +5,8 @@ set -e
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 TO=$(circleci env subst "${PARAM_TO}")
 VERSION=$(circleci env subst "${PARAM_VERSION}")
+CONFIG_FILE=$(circleci env subst "${PARAM_CONFIG_FILE}")
+PROJECT_TOKEN=$(circleci env subst "${PARAM_PROJECT_TOKEN}")
 # If for any reason the TO variable is not set, default to "World"
 echo "Hello ${TO:-World}!"
 
@@ -14,6 +16,10 @@ sudo apt-get update && sudo apt-get install -y rsync
 curl "https://update.brisktest.com/brisk/$VERSION/linux-amd64/brisk" -o brisk
 chmod +x brisk
 sudo mv brisk /usr/local/bin/brisk
+export BRISK_CI=true
+export BRISK_PROJECT_CONFIG_FILE=$CONFIG_FILE
+export BRISK_PROJECT_TOKEN=$PROJECT_TOKEN
+
 brisk version
 
 
